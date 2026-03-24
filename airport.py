66a2d9856060bdaf1bae1de343ecbc8d536ad073
juditsalv <judit.salvado.pont@estudiantat.upc.edu>
2026-03-24 12:04:22 +0100

    Updated airport.py to include only the Airport class and functions and added PlotAirports and MapAirports functions.

--- airport.py
+++ airport.py
@@ -1,16 +1,34 @@
 class Airport:
-    """Class representing an airport."""
-
-    def __init__(self, name, code, location):
+    def __init__(self, name, code, latitude, longitude):
         self.name = name
         self.code = code
-        self.location = location
+        self.latitude = latitude
+        self.longitude = longitude
 
-    def get_info(self):
-        return f"{self.name} ({self.code}) located in {self.location}."
+    def display_info(self):
+        return f"{self.name} ({self.code}): {self.latitude}, {self.longitude}"
 
-    def set_location(self, location):
-        self.location = location
+    @staticmethod
+    def plot_airports(airports):
+        import matplotlib.pyplot as plt
+        latitudes = [airport.latitude for airport in airports]
+        longitudes = [airport.longitude for airport in airports]
+        plt.scatter(longitudes, latitudes, marker='o')
+        plt.title('Airport Locations')
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        plt.show()
 
-    def __str__(self):
-        return f"Airport({self.name}, {self.code}, {self.location})"
+    @staticmethod
+    def map_airports(airports):
+        import folium
+        if not airports:
+            return None
+        first_airport = airports[0]
+        m = folium.Map(location=[first_airport.latitude, first_airport.longitude], zoom_start=5)
+        for airport in airports:
+            folium.Marker(
+                location=[airport.latitude, airport.longitude],
+                popup=airport.display_info()
+            ).add_to(m)
+        return m
